9102546853804021c0e111dce3051f44be456fdb
SaToGo <stoledano97@gmail.com>
2022-08-30 20:00:15 +0200

    Added style to every page, and new DOM elements

--- src/pages/coffee.js
+++ src/pages/coffee.js
@@ -1,7 +1,9 @@
 export function coffeeLoad(content) {
     const main = document.createElement('main');
     const pageTitle = document.createElement('h1');
+    const titleDiv = document.createElement('div')
 
+    titleDiv.className = 'titleDiv';
     pageTitle.innerText = "Coffee Selection";
     pageTitle.className = "title";
     
@@ -10,13 +12,17 @@ export function coffeeLoad(content) {
     const coffee1 = coffeeSetUp(coffee1Title(), coffee1description(), coffee1img());
 
     //Coffee2 Creation
-
+    const coffee2 = coffeeSetUp(coffee2Title(), coffee2description(), coffee2img());
 
     //Coffee3 Creation
+    const coffee3 = coffeeSetUp(coffee3Title(), coffee3description(), coffee3img());
 
     main.id = "main";
-    main.appendChild(pageTitle);
+    titleDiv.appendChild(pageTitle)
+    main.appendChild(titleDiv);
     main.appendChild(coffee1);
+    main.appendChild(coffee2);
+    main.appendChild(coffee3);
     content.appendChild(main);
 }
 
@@ -32,6 +38,7 @@ function coffeeSetUp(title, description, img) {
     const coffeeDescription = document.createElement('p');
     const coffeeImg = document.createElement('img');
 
+    coffee.className = "coffee__card";
     coffeeTitle.textContent = title;
     coffeeDescription.textContent = description;
     coffeeImg.src = img;
@@ -53,9 +60,33 @@ function coffee1Title(){
 }
 
 function coffee1description(){
-    return "Lose yourself in the steam of the best italian Coffee";
+    return "Lose yourself in the steam of the best italian Coffee.";
 }
 
 function coffee1img(){
     return "https://www.arte-latte.com/wp-content/uploads/2020/12/cappuccino1-min-scaled.jpg";
+}
+
+function coffee2Title(){
+    return "Milk Coffee";
+}
+
+function coffee2description(){
+    return "A Simple milk coffee for a good evening.";
+}
+
+function coffee2img(){
+    return "https://www.arte-latte.com/wp-content/uploads/2020/12/cafe-con-leche-min-scaled.jpg";
+}
+
+function coffee3Title(){
+    return "Latte";
+}
+
+function coffee3description(){
+    return "A latte where the art and the flavour combine to the maximum expression of the vibes.";
+}
+
+function coffee3img(){
+    return "https://www.arte-latte.com/wp-content/uploads/2020/12/latte1-min-scaled.jpg";
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,46 +1,112 @@
+import phoneIcon from '../img/phone.svg';
+import mailIcon from '../img/at.svg';
+import instaIcon from '../img/instagram.svg';
+import twitterIcon from '../img/twitter.svg';
+
 export function contactLoad(content){
 
     const main = document.createElement('main');
     const pageTitle = document.createElement('h1');
+    const titleDiv = document.createElement('div')
     
     const div = document.createElement('div');
 
-    const div1 = document.createElement('div');
-    const pageTitle1 = document.createElement('h2');
+    const aboutUsDiv = document.createElement('div');
+    const aboutUsTitle = document.createElement('h2');
     const pageDescription1 = document.createElement('p');
     
-    const div2 = document.createElement('div');
-    const pageTitle2 = document.createElement('h2');
-    const pageDescription2 = document.createElement('p');
+    const contactInformationDiv = document.createElement('div');
     
-    const div3 = document.createElement('div');
-    const pageTitle3 = document.createElement('h2');
-    const pageDescription3 = document.createElement('p');
+    const contactInformation = document.createElement('h2');
+    const contactPhoneMailDiv = document.createElement('div');
+    const phoneDiv = document.createElement('div')
+    const phone = document.createElement('p');
+    const phoneImg = document.createElement('img');
+    const mailDiv = document.createElement('div')
+    const mail = document.createElement('p');
+    const mailImg = document.createElement('img');
 
+    const contactSocialNetwork = document.createElement('h2');
+    const contactSocialNetworkDiv = document.createElement('div');
+    const twitterDiv = document.createElement('div')
+    const twitter = document.createElement('p');
+    const twitterImg = document.createElement('img');
+    const instaDiv = document.createElement('div')
+    const insta = document.createElement('p');
+    const instaImg = document.createElement('img');
+
+    
+
+    titleDiv.className = 'titleDiv';
     pageTitle.innerText = "Contact";
     pageTitle.className = "title";
 
-    pageTitle1.innerText = "About Us"
+
+    aboutUsDiv.className = "card";
+    aboutUsTitle.innerText = "About Us";
     pageDescription1.innerText = "This Shop is run by Coffee Lovers, we feel like we are flying through a new world when we smell a good coffee, and we want to make you feel the same.";
-    div1.appendChild(pageTitle1);
-    div1.appendChild(pageDescription1);
 
-    pageTitle2.innerText = "Mail";
-    pageDescription2.innerText = "thisMailIsFake@gmail.com";
-    div2.appendChild(pageTitle2);
-    div2.appendChild(pageDescription2);
 
-    pageTitle3.innerText = "Telephone";
-    pageDescription3.innerText = "111 11 11 11";
-    div3.appendChild(pageTitle3);
-    div3.appendChild(pageDescription3);
+    contactInformation.innerText = "Contact Information";
+    contactInformationDiv.className = "contact__Contact-Information";
 
-    div.appendChild(div1);
-    div.appendChild(div2);
-    div.appendChild(div3);
+    contactPhoneMailDiv.className = "contact__phone-mail";
+
+    mailDiv.className = "contact__icon-text";
+    mailImg.src = mailIcon
+    mailImg.className = "contact__Icon"
+    mail.innerText = "thisMailIsFake@gmail.com";
+
+    phoneDiv.className = "contact__icon-text";
+    phoneImg.src = phoneIcon;
+    phoneImg.className = "contact__Icon"
+    phone.innerText = "111 11 11 11";
+
+    contactSocialNetwork.innerText = "Social Network"
+    contactSocialNetwork.className = "contact__social-network"
+    contactSocialNetworkDiv.className = "contact__social-network-div"
+
+    twitterDiv.className = "contact__icon-text";
+    twitterImg.src = twitterIcon;
+    twitterImg.className = "contact__Icon"
+    twitter.innerText = "@thisTwitterIsFake";
+
+    instaDiv.className = "contact__icon-text";
+    instaImg.src = instaIcon;
+    instaImg.className = "contact__Icon"
+    insta.innerText = "@thisInstagramIsFake";
+
+
+
+    aboutUsDiv.appendChild(aboutUsTitle);
+    aboutUsDiv.appendChild(pageDescription1);
+    div.appendChild(aboutUsDiv);
+
+    mailDiv.appendChild(mailImg);
+    mailDiv.appendChild(mail);
+    phoneDiv.appendChild(phoneImg);
+    phoneDiv.appendChild(phone);
+    contactPhoneMailDiv.appendChild(mailDiv);
+    contactPhoneMailDiv.appendChild(phoneDiv);
+
+    twitterDiv.appendChild(twitterImg);
+    twitterDiv.appendChild(twitter);
+    instaDiv.appendChild(instaImg);
+    instaDiv.appendChild(insta);
+    contactSocialNetworkDiv.appendChild(twitterDiv);
+    contactSocialNetworkDiv.appendChild(instaDiv);
+
+    contactInformationDiv.appendChild(contactSocialNetwork);
+    contactInformationDiv.appendChild(contactSocialNetworkDiv);
+    contactInformationDiv.appendChild(contactInformation);
+    contactInformationDiv.appendChild(contactPhoneMailDiv);
+
+    div.appendChild(contactInformationDiv);
 
     main.id = "main";
-    main.appendChild(pageTitle);
+    
+    titleDiv.appendChild(pageTitle)
+    main.appendChild(titleDiv);
     main.appendChild(div);
 
     content.appendChild(main);
